1d63f440c1136bc434d78f51a234091b29a6f04d
FajlaRabby24 <fajlarabbyla24@gmail.com>
2026-08-31 13:37:39 +0600

    using builting exception for throwing error

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
 
 @Controller('songs')
-export class SongsController {}
+export class SongsController {
+  @Get()
+  findAll() {
+    try {
+      return 'This action returns all songs';
+    } catch (error) {
+      throw new HttpException(
+        'server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        {
+          cause: error,
+        },
+      );
+    }
+  }
+}
--- src/songs/songs.module.ts
+++ src/songs/songs.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SongsController } from './songs.controller.js';
+import { SongsService } from './songs.service.js';
 
 @Module({
-  controllers: [SongsController]
+  controllers: [SongsController],
+  providers: [SongsService]
 })
 export class SongsModule {}
