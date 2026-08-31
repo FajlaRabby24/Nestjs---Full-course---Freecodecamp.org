bf884dbd956c934dbae452d1a356234e1e7f7628
FajlaRabby24 <fajlarabbyla24@gmail.com>
2026-08-31 13:31:32 +0600

    feat: how to apply middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware.js';
+import { SongsController } from './songs/songs.controller.js';
 import { SongsModule } from './songs/songs.module.js';
 
 @Module({
@@ -8,4 +10,10 @@ import { SongsModule } from './songs/songs.module.js';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // consumer.apply(LoggerMiddleware).forRoutes('songs'); // * option 1
+    // consumer.apply(LoggerMiddleware).forRoutes({path: "songs", method: RequestMethod.POST}) // * option 2
+    consumer.apply(LoggerMiddleware).forRoutes(SongsController); // * option 3
+  }
+}
